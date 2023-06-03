4b73d86007374167ae37c0f0e26fee602b6689c8
warrenshiv <warrenshiv@gmail.com>
2023-06-03 10:41:38 +0300

    Modified the StudentController

--- api/app/Http/Controllers/Api/StudentController.php
+++ api/app/Http/Controllers/Api/StudentController.php
@@ -27,5 +27,36 @@ class StudentController extends Controller
     }
 
     public function store(Request $request){
-        $validator = Validator::make();
+        $validator = Validator::make($request->all(), [
+           'name' => 'required|string|max:191',
+           'course' => 'required|string|max:191',
+           'email' => 'required|email|max:191',
+           'phone' => 'required|digits:10',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ], 422);
+        }else{
+            $student = Student::create([
+                'name' => $request -> name,
+                'course' => $request -> course,
+                'email' => $request -> email,
+                'phone' => $request -> phone,
+            ]);
+            if($student){
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Student Created Successfully"
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something Went Wrong"
+                ], 500);
+            }
+        }
     }
+}
